49170832f254ae0debe115cd110428eeef9648e5
milfr <miller.francis@gmail.com>
2016-09-19 15:27:24 -0400

    Learning Phase : using namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 // Let's say hello to the world!
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
 	std::cout << "Well hello there! I'm the world!\n";
+	// or using the namespace ...
+	cout << "- Hello to you World!\n";
 }
